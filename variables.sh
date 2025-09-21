6b97911a160f45fa2a259c195902502ef8c72b13
mail2chandrasheker <mail2chandrasheker@gmail.com>
2025-09-21 07:31:10 +0530

    added variable file

---
+++ variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+person1=john
+person2=raheem
+
+echo "$person1:: h1  $person2 ,how are you"
+echo "$person2::hello $person2, i am fine ,how are you"
+echo "$person1::what are you learning recently"
+echo "$person2:: i am learning shell script $person1"
