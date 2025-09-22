77eba269f4cd0cf418641c8942b033133de0868b
mail2chandrasheker <mail2chandrasheker@gmail.com>
2025-09-22 22:01:47 +0530

    updated

--- 10-installation.sh
+++ 10-installation.sh
@@ -4,12 +4,15 @@ USERID=$(id -u)
 
 if [ $USERID -ne 0 ]; then
    echo "ERROR: Please run this sccript with root privelege"
+   exit 1 # failure is other than 0
 fi
 dnf install mysql -y
 
 if [ $? -ne 0 ]; then
    echo "ERROR: installing mysql is failure"
+   exit 1
 else
    echo "Installing mysql is success"
+   
 
 fi
