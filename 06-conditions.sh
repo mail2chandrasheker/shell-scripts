3ff85fd40f7e95bd4ae59cc4d532bf296c82c712
mail2chandrasheker <mail2chandrasheker@gmail.com>
2025-09-22 20:41:59 +0530

    added conditions programm

--- 06-conditions.sh
+++ 06-conditions.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 NUMBER=$1
-if [$NUMBER -lt 10 ]; then
+if [ $NUMBER -lt 10 ]; then
    echo "Given number $NUMBER is less than 1o"
 else
    echo "Given number $NUMBER is greater tha or equal to 1o"
 
-# -gt ,-eq , -lt, -ne
