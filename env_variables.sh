85a5ca6df813b274a06b2ba7b92396b0d34a73f2
mail2chandrasheker <mail2chandrasheker@gmail.com>
2025-09-27 00:17:44 +0530

    added new file

---
+++ env_variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "Home directory: $HOME"
+echo "Current user: $USER"
+echo "Present working directory: $PWD"
+echo "Shell: $SHELL"
+echo "Current path: $PATH"
+
+export GREETING="HELLO FRIEND!"
+echo $GREETING
