41110b172ccc5a74186eca196b5796d09f1bd684
mail2chandrasheker <mail2chandrasheker@gmail.com>
2025-09-27 18:59:39 +0530

    updated installation

---
+++ installation_from_script.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#first check the user  is root or normal user
+userid=$(id -u)
+
+if [ $userid  -ne 0 ]; then
+    echo "Error:: please run this script with root user"
+    exit 1
+fi
+
+dnf install mysql -y
+
+if [ $? -ne 0 ]; then
+   echo  "Error: Installing mysql is failed"
+    exit 1
+else
+   echo "Installating mysql is success"
+fi
