ce8faaa46ac4da41dfd0e73b1943bff246128b87
mail2chandrasheker <mail2chandrasheker@gmail.com>
2025-09-27 14:22:47 +0530

    userid added

--- userid.sh
+++ userid.sh
@@ -3,12 +3,15 @@ USERID=$(id -u)
 
 if [ $USERID -ne 0 ] ;then
     echo "ERROR::Please run this cript with root privelage"
+    exit 1
 fi
 
 dnf install mysql -y
 
 if [ $? -ne 0 ];then
     echo "ERROR:: installing mysql is failure"
+    exit 1
 else
     echo "Installing mysql is success"
+    exit 0
 fi
