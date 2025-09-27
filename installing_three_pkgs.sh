c4986f6cc10e22155a040c1b011d5079f08d4016
mail2chandrasheker <mail2chandrasheker@gmail.com>
2025-09-27 21:40:03 +0530

    install 3 packages

--- installing_three_pkgs.sh
+++ installing_three_pkgs.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 
 #validate the user is root or normla
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
 
 userid=$(id  -u)
 
 if [ "$userid"   -ne  0 ] ; then
-   echo "Error:: you   are  not root user"
+   echo -e "$R Error:: you   are  not root user $N"
    exit 1
 fi
 
 validate(){
     if [ $1 -ne  0 ]; then
-        echo "Error:: insatalling $2 is failed"
+        echo -e "$R Error:: insatalling $2 is failed $N"
     else
-        echo "installing $2 is success"
+        echo -e "$G installing $2 is success $N"
     fi
 }
 
