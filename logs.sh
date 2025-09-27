82e254973925ca3ef53166199c1c6148282d2799
mail2chandrasheker <mail2chandrasheker@gmail.com>
2025-09-27 22:08:11 +0530

    logs

---
+++ logs.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+#validate the user is root or normla
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+Y="\e[033m"
+userid=$(id  -u)
+
+logs_folder="/var/log/shell-script"
+script_name=$(echo $0 | cut -d "." -f1 )
+log_file="$logs_folder/$script_name.log"
+
+mkdir -p $log_folder
+
+echo "script started executed at:$(date)"
+if [ "$userid"   -ne  0 ] ; then
+   echo -e "$R Error:: you   are  not root user $N"
+   exit 1
+fi
+
+validate(){
+    if [ $1 -ne  0 ]; then
+        echo -e "$R Error:: insatalling $2 is failed $N"
+    else
+        echo -e "$G installing $2 is success $N"
+    fi
+}
+
+dnf list installed mysql &>>$log_file
+if [ $? -ne 0 ];then
+  dnf install mysql -y &>>$log_file
+  validate $? "MYSQL"
+else
+    echo -e "Mysql already exist ...$Y SKipping$N"
+fi
+
+dnf list installed nginx &>>$log_file
+if [ $? -ne 0 ] ; then
+   dnf install nginx -y &>>$log_file
+   validate $? "Nginx"
+else
+   echo -e "nginx already exist...$Y Skipping$N"
+fi
